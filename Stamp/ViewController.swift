c6559411cc19c6037cb089b66fccc17196d08a5a
BuildTools <unconfigured@null.spigotmc.org>
2019-01-03 23:08:38 +0900

    Initial Commit

---
+++ Stamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stamp
+//
+//  Created by 西村　元晴 on 2019/01/03.
+//  Copyright © 2019年 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
