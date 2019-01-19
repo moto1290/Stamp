a0af88ca01cfa9294e89c952c85ecd5ad0eeecdf
BuildTools <motosan_nsmr@icloud.com>
2019-01-19 19:57:11 +0900

    2回目のコミット

--- Stamp/ViewController.swift
+++ Stamp/ViewController.swift
@@ -8,12 +8,96 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imageNameArray: [String] = ["hana.png", "hoshi.png", "onpu.png", "shitumon.png"]
+    
+    var imageIndex: Int = 0
+    
+    @IBOutlet var haikeiImageView: UIImageView!
+    
+    var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func selectedFirst() {
+        imageIndex = 1
+    }
+    @IBAction func selectedSecond() {
+        imageIndex = 2
+    }
+    @IBAction func selectedThird() {
+        imageIndex = 3
+    }
+    @IBAction func selectedForth() {
+        imageIndex = 4
+    }
+    
+
+    @IBAction func back(){
+        self.imageView.removeFromSuperview()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch: UITouch = touches.first!
+        let location: CGPoint =  touch.location(in: self.view)
+        print(location)
+        if imageIndex != 0 {
+            
+            imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 0, height : 40))
+            
+            let image: UIImage = UIImage(named: imageNameArray[imageIndex - 1])!
+            imageView.image = image
+            
+            imageView.center = CGPoint(x: location.x, y: location.y)
+            
+            self.view.addSubview(imageView)
+            
+        }
+    }
+    
+    
+    
+        
+        
+    
+    
+    
+    @IBAction func selectbackground() {
+        
+        
+        let imagePickerController: UIImagePickerController = UIImagePickerController()
+        
+        imagePickerController.sourceType = UIImagePickerController.SourceType.photoLibrary
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = info[.originalImage] as? UIImage
+        
+        haikeiImageView.image = image
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func save() {
+        
+        let rect:CGRect = CGRect(x: 0, y: 30, width: 320, height: 380)
+        UIGraphicsBeginImageContext(rect.size)
+        self.view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let capture = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        UIImageWriteToSavedPhotosAlbum(capture!,nil, nil, nil)
+    }
+    
+    
 
 
 }
